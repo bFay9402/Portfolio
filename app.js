20f20bd1cdb7289b9a6d597986b6698e0a723a59
bFay9402 <bfay9402@gmail.com>
2020-11-03 16:37:32 -0500

    implemented materialize built nav, banner, and portfolio cards

---
+++ app.js
@@ -0,0 +1,11 @@
+// side nav
+const sideNav = document.querySelector('.sidenav');
+M.Sidenav.init(sideNav, {});
+
+// scroll spy
+const ss = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(ss, {});
+
+// parallax
+const parallax = document.querySelectorAll('.parallax');
+M.Parallax.init(parallax, {}); 
